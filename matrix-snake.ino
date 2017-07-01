abd07e7389b800dc6db377a20f277ff5e2410c9f
Lokhin Wu <lokhin.wu@live.com>
2017-07-01 14:55:07 -0400

    Snake movement

--- matrix-snake.ino
+++ matrix-snake.ino
@@ -3,9 +3,14 @@
 unsigned long delayTime = 100;
 
 int level;
-int joyX = 0;
-int joyY = 1;
-int joyVal;
+int snakeSpeed;
+int snakeDirection; // 1 = UP; 2 = RIGHT; 3 = DOWN; 4 = LEFT
+int snakeLength;
+int foodX, foodY;
+int joyX = A0;
+int joyY = A1;
+int joyBtn = 2;
+int joyValX, joyValY;
 int DIN = 12;
 int CLK = 11;
 int CS = 10;
@@ -21,9 +26,12 @@ byte one[8] = {0x00, 0x18, 0x18, 0x38, 0x18, 0x18, 0x18, 0x7E};
 byte two[8] = {0x00, 0x3C, 0x66, 0x06, 0x0C, 0x30, 0x60, 0x7E};
 byte three[8] = {0x00, 0x3C, 0x66, 0x06, 0x1C, 0x06, 0x66, 0x3C};
 byte fill[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+byte grid[8][8];
 
-void setup() {
-//Serial.begin(9600);
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(joyBtn, INPUT_PULLUP);
   lc.shutdown(0, false);
   lc.setIntensity(0, 15);
   lc.clearDisplay(0);
@@ -43,13 +51,14 @@ void setup() {
   printByte(one);
 }
 
-void loop() {
-  joyVal = analogRead(joyX);
-  if (joyVal <= 341) {
+void loop() 
+{
+  joyValX = analogRead(joyX);
+  if (joyValX <= 341) {
     if (!(level >= 3)) {
       level +=1;
     }
-  } else if (joyVal >= 682) {
+  } else if (joyValX >= 682) {
      if (!(level <= 1)) {
       level -=1;
     }
@@ -61,14 +70,102 @@ void loop() {
   } else if (level == 3) {
     printByte(three);
   }
+  if (digitalRead(joyBtn) == LOW) {
+    startGame();
+  }
   delay(200);
+}
+
+void startGame()
+{
+  int snakeHeadX = 6;
+  int snakeHeadY = 2;
+  snakeDirection = 0;
+  snakeLength = 1;
+  initGrid();
+  makeFood();
+  while(inGrid(snakeHeadX, snakeHeadY)) {
+    joyValX = analogRead(joyX);
+    if (joyValX <= 341) {
+      snakeDirection = 2; 
+    } else if (joyValX >= 682) {
+      snakeDirection = 4;
+    }
+    joyValY = analogRead(joyY);
+    if (joyValY <= 341) {
+      snakeDirection = 1;
+    } else if (joyValY >= 682) {
+      snakeDirection = 3;
+    }
+    if (snakeDirection == 1) {
+      grid[snakeHeadX - 1][snakeHeadY] = 1;
+      grid[snakeHeadX][snakeHeadY] = 0;
+      snakeHeadX -= 1;  
+    } else if (snakeDirection == 2) {
+      grid[snakeHeadX][snakeHeadY + 1] = 1;
+      grid[snakeHeadX][snakeHeadY] = 0;
+      snakeHeadY += 1;  
+    } else if (snakeDirection == 3) {
+      grid[snakeHeadX + 1][snakeHeadY] = 1;
+      grid[snakeHeadX][snakeHeadY] = 0;
+      snakeHeadX += 1;  
+    } else if (snakeDirection == 4) {
+      grid[snakeHeadX][snakeHeadY - 1] = 1;
+      grid[snakeHeadX][snakeHeadY] = 0;
+      snakeHeadY -= 1;   
+    }
+    if (snakeHeadX == foodX && snakeHeadY == foodY) {
+      makeFood();
+      snakeLength += 1;
+    }
+    if (inGrid(snakeHeadX, snakeHeadY)) {
+      drawGrid();
+    }
+    if (snakeDirection != 0) {
+      lc.setLed(0, foodX, foodY, 1);
+    }
+    delay(500 / level);
+  }
+}
+
+void makeFood()
+{
+  foodX = random(0, 8);
+  foodY = random(0, 8);
+}
+
+bool inGrid(int x, int y)
+{
+  return ((x >= 0 && x <= 7) && (y >= 0 && y <= 7));
+}
+
+void initGrid()
+{
+  int i = 0;
+  int j = 0;
+  for(i = 0; i < 8; i++) {
+    for(j = 0; j < 8; j++) {
+      grid[i][j] = 0;
+    }  
+  }
+  grid[6][2] = 1;
+}
+
+void drawGrid()
+{
+  int i = 0;
+  int j = 0;
+  for(i = 0; i < 8; i++) {
+    for(j = 0; j < 8; j++) {
+      lc.setLed(0, i, j, grid[i][j]);
+    }  
+  }
 }
 
 void printByte(byte character [])
 {
   int i = 0;
-  for(i=0;i<8;i++)
-  {
-    lc.setRow(0,i,character[i]);
+  for(i = 0; i < 8; i++) {
+    lc.setRow(0, i, character[i]);
   }
 }
